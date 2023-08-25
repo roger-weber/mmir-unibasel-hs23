42f94c1db2376e756e5208ef0aa68cc27c6f2b7f
Roger.dev-at-649134713833 <roger.weber@gmail.com>
2023-08-25 08:04:59 +0200

    small changes

--- utils/output/table.py
+++ utils/output/table.py
@@ -3,8 +3,11 @@ from tabulate import tabulate
 from itertools import islice
 import builtins
 
-def print(rows: list[list[str]], headers: list[str], max_rows: int = 20):
+def print(rows: list[list[str]], headers: list[str], max_rows: int = 20, format: str = 'text'):
     if not rows or not headers: return
-    # display(Markdown(tabulate(islice(rows, max_rows), headers, tablefmt="pipe")))
-    builtins.print(tabulate(islice(rows, max_rows), headers, tablefmt="github"))
+    if format == 'text':
+        builtins.print(tabulate(islice(rows, max_rows), headers, tablefmt="github"))
+        builtins.print()
+    else:
+        display(Markdown(tabulate(islice(rows, max_rows), headers, tablefmt="pipe")))
 
